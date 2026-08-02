1a44a4dcf965e3a48a5a9c87c026e7aefab2ee5c
eswar <koradaeswar5599@gmail.com>
2026-08-02 12:31:34 +0530

    first-api-call

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const connectDb = require('./config/database.js');
+const User = require('./models/user.js')
 
 const app = express();
+
+app.post('/singup', (req, res)=>{
+
+    const userObj = {
+        firstName : 'Eswar',
+        secondName : 'Korada'
+    }
+
+    const user = new User(userObj);
+
+    user.save();
+
+    res.send(200);
+
+})
 
 connectDb().then(() => {
     app.listen(5599);
